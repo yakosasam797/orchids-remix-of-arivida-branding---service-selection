7459de5fbc00e2d7e5a336dba92697001bf30135
yakosasam797 <yakshithsaravu797@gmail.com>
2026-02-20 00:00:32 +0530

    Add Google Tag Manager (GTM-P28ZLF5Z) fresh install

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,6 +51,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P28ZLF5Z');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
         {/* DNS Prefetch and Preconnect for faster font loading */}
         <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
         <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
@@ -73,6 +84,16 @@ export default function RootLayout({
         <meta name="rating" content="general" />
       </head>
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P28ZLF5Z"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
 
         <Script
           id="orchids-browser-logs"
