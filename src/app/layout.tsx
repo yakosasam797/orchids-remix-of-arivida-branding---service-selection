67746bb434edad567e79700ffb5dd2ec484a056d
yakosasam797 <yakshithsaravu797@gmail.com>
2026-02-19 16:12:58 +0530

    Add Google Tag Manager (GTM-P28ZLF5Z)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,6 +51,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P28ZLF5Z');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
         {/* Google tag (gtag.js) - Google Ads */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=AW-17960269016"
@@ -86,6 +97,16 @@ export default function RootLayout({
         <meta name="rating" content="general" />
       </head>
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P28ZLF5Z"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Script
           id="orchids-browser-logs"
           src="https://slelguoygbfzlpylpxfs.supabase.co/storage/v1/object/public/scripts/orchids-browser-logs.js"
